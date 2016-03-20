b1784cb880568ec5e143fe47f25dce17eec24809
Oğuz Köroğlu <korogu@hotmail.com>
2016-03-20 12:34:28 +0200

    MVC 5 Application Created with Login/Register/ChangePassword Functions and Unit Tests for that

---
+++ NationalCriminalDB/NationalCriminalDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NationalCriminalDB.Startup))]
+namespace NationalCriminalDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
